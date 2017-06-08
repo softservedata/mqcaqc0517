b26d63fee078a31e6e92468fe772f84d717b8680
kpred <chrystya88@gmail.com>
2017-06-08 11:05:31 +0300

    new

--- src/HWpackage/ComparingArrays.java
+++ src/HWpackage/ComparingArrays.java
@@ -5,8 +5,15 @@ import java.util.Arrays;
 public class ComparingArrays {
 
 	public static void main(String[] args) {
-		int[] array1 = { 2, 4, 0, 8, 6 };
+		int[] array1 = { 6, 4, 2 };
 		int[] array2 = { 2, 4, 6 };
+
+		Arrays.sort(array1);
+		Arrays.sort(array2);
+
+		if (array1.length != array2.length) {
+			System.out.println("not equil");
+		}
 
 		boolean result = Arrays.equals(array1, array2);
 
