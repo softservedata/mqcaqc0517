1dcb276aad3e19246ae312a71adc6c5783d99f85
OL <olaytaruk@yahoo.com>
2017-06-14 00:14:08 +0300

    small fix: added calculation for second rectangle.

--- lesson4_hw/src/apps/AppHW4.java
+++ lesson4_hw/src/apps/AppHW4.java
@@ -4,11 +4,12 @@ import shapes.Figure;
 import shapes.Rectangle;
 import shapes.Square;
 
-public class App {
+public class AppHW4 {
 	public static void main(String[] args) {
-		Figure shapes[] = new Figure[2];
+		Figure shapes[] = new Figure[3];
 		shapes[0] = new Rectangle(200, 8);
-		shapes[1] = new Square(25);
+		shapes[1] = new Rectangle(10, 45);
+		shapes[2] = new Square(25);
 		
 		for(int i=0; i < shapes.length; i++) {
 			System.out.println("Area of " + shapes[i].name() + " is: " + shapes[i].area());
