7504e081a3e9ebcefd9bbcfa136c3f294f4166f5
OL <olaytaruk@yahoo.com>
2017-06-18 14:16:24 +0300

    small changes

--- Selenium_HW8/tst/com/softserve/edu/LoginCheck.java
+++ Selenium_HW8/tst/com/softserve/edu/LoginCheck.java
@@ -11,33 +11,31 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import junit.framework.Assert;
+import junit.framework.Assert; // ?
 
+@SuppressWarnings("deprecation")
 public class LoginCheck { 
 	@Test
 	public void testSelIdeRec01() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium-browser/chromedriver");
 	    WebDriver driver = new ChromeDriver();
 	   	
-	    driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// go to site:
 	   	driver.get("http://regres.herokuapp.com");
-		
+		// login:
 		driver.findElement(By.id("login")).clear();
 		driver.findElement(By.id("login")).sendKeys("work");
 		driver.findElement(By.id("password")).clear();
 		driver.findElement(By.id("password")).sendKeys("qwerty");
 		driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
-			
-		//String actual = driver.findElement(By.cssSelector("h3")).getText();
-		String actual = driver.findElement(By.cssSelector("div[class='col-md999-7 col-xs-12'] > h3")).getText();
-		Assert.assertTrue(actual.contains("Децентралізований майновий реєстр"));
-		// assertEquals(driver.findElement(By.cssSelector("h3")).getText(), "Децентралізований майновий реєстр");
-		//String actual = driver.findElement(By.cssSelector("a[name='selenium_ide'] > p")).getText(); // find web element. getText()- get a text between tags.
+		// validate that text related to site is present, else fail:	
+		String actual = driver.findElement(By.cssSelector("div[class='col-md-7 col-xs-12'] > h3")).getText(); // find web element. getText()- get a text between tags.
+		Assert.assertTrue(actual.contains("Децентралізований майновий реєстр")); // ?
+		// sign out:
 		driver.findElement(By.xpath("//div[@id='header']/div[2]/div/div/button[2]")).click();
 	    driver.findElement(By.linkText("Вихід")).click();
-		
-		driver.quit(); // close browser and stop web server.
+		// close web browser and shutdown Webdriver
+		driver.quit();
 	}
-
 }
