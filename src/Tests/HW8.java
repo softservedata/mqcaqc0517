f20d472d96b8474e1851100c38cd6e4c1cddc91f
kpred <chrystya88@gmail.com>
2017-06-21 17:12:36 +0300

    HW8

--- src/Tests/HW8.java
+++ src/Tests/HW8.java
@@ -8,7 +8,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.sun.org.apache.bcel.internal.generic.Select;
 
 public class HW8 {
 
@@ -18,6 +22,10 @@ public class HW8 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		//
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		driver = new ChromeDriver( options );
+		//
 		driver.get("http://regres.herokuapp.com/login");
 		
 		driver.findElement(By.id("login")).clear();
@@ -26,11 +34,28 @@ public class HW8 {
 		driver.findElement(By.id("password")).sendKeys("qwerty");
 		driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
 		
-
-		String actual = driver.findElement(By.cssSelector(".btn.btn-primary.btn-sm")).getText();
+		//String actual = driver.findElement(By.cssSelector(".btn.btn-primary.btn-sm")).getText();
 		
-		Assert.assertTrue(actual.contains("work"));
-		driver.quit();
+		//Assert.assertTrue(actual.contains("work"));
+		
+					
+		driver.findElement(By.xpath(".//*[@id='navigationbar']/ul/li[2]/a")).click();
+		
+				
+		driver.findElement(By.cssSelector("a[href='/administrator/users/get-all-users']")).click();
+		
+		driver.findElement(By.id("inputIndex5")).clear();
+		driver.findElement(By.id("inputIndex5")).sendKeys("admin@admin.com");
+		driver.findElement(By.id("bth-search")).click();
+		//driver.findElement(By.cssSelector(".email.sorting")).getText();
+        String actual = driver.findElement(By.cssSelector(".email.sorting")).getText();
+        
+        System.out.println(actual);
+        
+        Assert.assertTrue(actual.contains("admin@admin.com"));
+				
+		
+		driver.close();
 	}
 
 }
