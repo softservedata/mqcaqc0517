d8941104d6c8fb246eaf0baba81ca1379f548fb0
oll86 <olaytaruk@yahoo.com>
2017-06-28 18:15:32 +0300

    added old files for hw6&7

---
+++ HW7/AppHW7.java
@@ -0,0 +1,40 @@
+/*
+ * Homework:
+Нехай задана стрічка з фрагментом html коду, яка містить кілька тегів <p>text</p>.
+Сформувати колекцію лише із вмістимого параграфів. Вмістиме інших тегів ігнорувати.
+Вивести колекцію на консоль. */
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AppHW7 {
+
+	public static void main(String[] args) {
+				
+		String text = "<!DOCTYPE html> <html> <head> <title>Page Title</title>"+
+		"</head> <body> <h1>This is a Heading</h1> " +
+		"<p>This is a paragraph 1.</p>" +
+		"<p>This is a paragraph 2.</p>" +
+		"</body> </html>";
+		
+		String pattern = "(p>[^<]+<)";
+		
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(text);
+
+		m.reset();
+		
+		List<String> result = new ArrayList<>();
+		while (m.find()) {
+			
+			result.add(text.substring(m.start()+2, m.end()-1).trim());
+		}
+		
+		for(String i:result)
+			System.out.println(i);
+	}
+
+}
